f7497ceb5c93aa8f762e1abbbfa0c9cdb45d94c4
johanbjerke <johanbjerke@gmail.com>
2019-04-18 18:52:35 +0100

    SSE 2.4 version as of today.

--- appserver/static/dashboard.js
+++ appserver/static/dashboard.js
@@ -11,7 +11,7 @@ require([
 ], function(_, $, mvc, TableView) {
     var CustomIconRenderer = TableView.BaseCellRenderer.extend({
         canRender: function(cell) {
-            return cell.field === 'Details' || cell.field === 'Description' || cell.field === 'Title' || cell.field === 'Detection Search';
+            return cell.field === 'Details' || cell.field === 'Description' || cell.field === 'Title' || cell.field === 'Detection Search' || cell.field === 'Data Availability' || cell.field === 'Enabled';
 			//return cell.field === 'Details';
         },
         render: function($td, cell) {
@@ -32,12 +32,22 @@ require([
 
 				//cell.value=details;
 				$td.html(details);
-			} else if (cell.field === 'Description' || cell.field === 'Title' || cell.field === 'Detection Search') {
+			} else if (cell.field === 'Details' || cell.field === 'Description' || cell.field === 'Title' || cell.field === 'Detection Search') {
         		//This section effectively parses the html tags so they are not shown in the table. It also adds highlights to any search filter.
 				var cellvalue = cell.value;
         		var searchfilter=$(".splunk-textinput input[type='text']").val();
         		if (typeof searchfilter.length!="undefined" && searchfilter.length>0) {
-        			var cellvalue = cellvalue.replace(RegExp(searchfilter, "gi"), '<span class="highlight">'+searchfilter+'</span>');
+        			cellvalue = cellvalue.replace(RegExp(searchfilter, "gi"), '<span class="highlight">'+searchfilter+'</span>');
+				}
+
+        		//console.log(cellvalue)
+				$td.html(cellvalue);
+			} else {
+        		var cellvalue = cell.value;
+        		if (cell.field === 'Enabled') {
+        			cellvalue = cellvalue.replace(RegExp("Yes", "gi"), '<span class="green">Yes</span>');
+				} else if (cell.field === 'Data Availability') {
+        			cellvalue = cellvalue.replace(RegExp("Good", "gi"), '<span class="green">Good</span>');
 				}
         		//console.log(cellvalue)
 				$td.html(cellvalue);
@@ -47,6 +57,45 @@ require([
     mvc.Components.get('content_table').getVisualization(function(tableView){
         // Register custom cell renderer, the table will re-render automatically
         tableView.addCellRenderer(new CustomIconRenderer());
+    });
+    var MitreMapTableRenderer = TableView.BaseCellRenderer.extend({
+        canRender: function(cell) {
+            return true;
+        },
+        render: function($td, cell) {
+			var cellvalue = cell.value;
+
+			if(cellvalue != null) {
+				values = [];
+				$.each(cellvalue, function( i, val ) {
+					values[i]=val;
+
+				});
+				var celltext=values[0];
+				var colorhex=values[1];
+				var bgcoloropacity=values[2];
+				var tooltip=values[3];
+
+				$td.css("background-color","rgb("+hexToRgb(colorhex)+","+bgcoloropacity+")")
+
+				$td.html(_.template('<span data-toggle="tooltip" data-placement="bottom" title="<%- tooltip%>"><%- celltext%></span>', {
+						tooltip: tooltip,
+						celltext: celltext
+
+				}));
+
+				$td.children().tooltip({ html: 'true' })
+				//console.log(mitre_technique+" "+mitre_technique_count_total)
+
+
+			} else {
+				$td.html(cell.value);
+			}
+        }
+    });
+     mvc.Components.get('mitremaptable').getVisualization(function(tableView){
+        // Register custom cell renderer, the table will re-render automatically
+        tableView.addCellRenderer(new MitreMapTableRenderer());
     });
 });
 
@@ -62,7 +111,7 @@ require(["jquery",
         $, Ready) {
 		$("label:contains('Split by')").prop('title', 'This option selects which field to split the X-axis by.');
 		$("label:contains('Phase')").prop('title', '<p>Current: Content is installed, the data is in Splunk and it is enabled.</p><p>Phase 1: Content is installed, the data is in Splunk and but it is disabled.</p><p>Phase 2: Content is installed, the data is not in Splunk and it is disabled.</p><p>Selected Sourcetypes: This highlight where the selected sourcetypes would map to what is on the X-axis. .</p>');
-		$("label:contains('Status')").prop('title', '<p>Current: Content is installed, the data is in Splunk and it is enabled.</p><p>Phase 1: Content is installed, the data is in Splunk and but it is disabled.</p><p>Phase 2: Content is installed, the data is not in Splunk and it is disabled.</p><p>Selected Sourcetypes: This highlight where the selected sourcetypes would map to what is on the X-axis. .</p>');
+		$("label:contains('Status')").prop('title', '<p>Active: Content is enabled and the data required is available in Splunk.</p><p>Available: Content is disabled and the data required is available in Splunk.</p><p>Needs data: We need more data to enable the content</p><p>Selected: This highlights where the selected data sources would map to what is on the X-axis. .</p>');
 		$("label:contains('Sourcetype origin')").prop('title', '<p>Installed Sourcetypes: The chart is filtered to Sourcetypes that are currently installed.</p><p>All Sourcetypes: The chart is filtered to sourcetypes that are either installed or are referred to in the content search string.</p><p>Sample Sourcetypes: The chart is filtered to sourcetypes that are provided with the app. This list represents a large proportion of the sourcetypes available on Splunkbase. This setting will allow you to run analytics without having any Add-ons or data in Splunk.</p>');
 		$("label:contains('Sourcetype selection')").prop('title', 'Select which Sourcetypes to highlighted in the "Selected" field');
 		$("label:contains('Data')").prop('title', 'Filters the list to show if we have data for the content.');
@@ -74,6 +123,13 @@ require(["jquery",
         $("label:contains('Sourcetype filter')").prop('title', 'Filters to show only content that is mapped to the selected Sourcetypes.');
 		$("label:contains('Add-On')").prop('title', 'Filters the list to show only content that is mapped to the selected Add-On.');
         $("label:contains('Sourcetype and Add-On')").prop('title', 'Toggle to show the list of Sourcetypes and Add-Ons associated with the content.');
+        $("label:contains('Featured')").prop('title', 'Featured searches are those that come recommended by Splunk\'s Security SMEs.');
+        $("label:contains('Bookmark Status')").prop('title', 'Filters the list to show only content that has the selected Bookmark Status');
+        $("label:contains('Bookmarked and Featured')").prop('title', 'Toggle to show only content that is only bookmarked or featured');
+        $("label:contains('Data Source')").prop('title', 'The data sources that power the content. These are mapped to individual technologies.');
+        $("label:contains('Data Source selection')").prop('title', 'Highlight which content uses the selected Data Sources. Data Sources are mapped to individual technologies.');
+        $("label:contains('Data Source Category')").prop('title', 'The data category that powers the content. A data category is granular and details which types of events are required to enable the content.');
+        $("label:contains('Journey')").prop('title', 'This represents where the content sits in the Splunk Security Journey.');
 		$("label:contains('Search Query')").prop('title', 'Toggle to show the actual SPL search for the content.');
 		$("label:contains('Search Filter')").prop('title', 'The filter does a case insensitive text match. There are implied wildcards before and after the input string. You can also specify underscore ( _ ) characters for a single character match.');
 		$("label:contains('MITRE Tactic')").prop('title', 'MITRE Tactics represent the "why" of an ATT&CK technique. It is the adversary’s tactical objective: the reason for performing an action. See the MITRE reference link at the top of the page for more details.');
@@ -82,7 +138,30 @@ require(["jquery",
 		$("label:contains('NIST')").prop('title', 'The NIST Cybersecurity Framework presents a listing of Functions, Categories, Subcategories and Informative References (standards, guidelines, and practices) that describe specific cybersecurity activities. The mapping in this app represents the Function and Category elements of the framework. See the NIST Cybersecurity Framework reference link at the top of the page for more details.');
 		$("label:contains('CIS')").prop('title', 'The CIS Controls are a prioritized set of controls and actions to defend against pervasive cyber threats. See the CIS Controls reference link at the top of the page for more details.');
 		$("label[title]").tooltip({ html: 'true' })
+		$("#row1_col5 .panel-title:contains('Stage 1')").prop('title', '<h3>Collection</h3><p>Collect basic security logs and other machine data from your environment.</p>');
+		$("#row1_col5 .panel-title:contains('Stage 2')").prop('title', '<h3>Normalization</h3><p>Apply a standard security taxonomy and add asset and identity data.</p>');
+		$("#row1_col5 .panel-title:contains('Stage 3')").prop('title', '<h3>Expansion</h3><p>Collect additional high fidelity data sources like endpoint activity and network metadata to drive advanced attack detection.</p>');
+		$("#row1_col5 .panel-title:contains('Stage 4')").prop('title', '<h3>Enrichment</h3><p>Augment security data with intelligence sources to better understand the context and impact of an event.</p>');
+		$("#row1_col5 .panel-title:contains('Stage 5')").prop('title', '<h3>Automation and Orchestration</h3><p>Establish a consistent and repeatable security operation capability.</p>');
+		$("#row1_col5 .panel-title:contains('Stage 6')").prop('title', '<h3>Advanced Detection</h3><p>Apply sophisticated detection mechanisms including machine learning.</p>');
+        //$(".panel-title[title]").tooltip({ html: 'true'})
     })
+
+$(".toggle-tab[data-token='row1_col4']").on('click', function () {
+        setTimeout(function () {
+            $("#row1_col5 .panel-title[title]").tooltip({html: 'true', animation: 'true'}).tooltip('show');
+				setTimeout(function () {
+					$("#row1_col5 .panel-title[title]").tooltip('hide');
+				}, 4000)
+        }, 500)
+});
+
+
+//$( ".toggle-tab[data-token='row1_col1']" ).on('click',function(){$("#row1_col5 .panel-title[title]").tooltip('dispose');});
+//$( ".toggle-tab[data-token='row1_col2']" ).on('click',function(){$("#row1_col5 .panel-title[title]").tooltip('dispose');});
+//$( ".toggle-tab[data-token='row1_col3']" ).on('click',function(){$("#row1_col5 .panel-title[title]").tooltip('dispose');});
+//$( ".toggle-tab[data-token='row1_col8']" ).on('click',function(){$("#row1_col5 .panel-title[title]").tooltip('dispose');});
+
 require(['jquery','underscore','splunkjs/mvc', 'bootstrap.tab', 'splunkjs/mvc/simplexml/ready!'],
 		function($, _, mvc){
 
@@ -203,6 +282,7 @@ require(['jquery','underscore','splunkjs/mvc', 'bootstrap.tab', 'splunkjs/mvc/si
 		tokens.set(tabToken, '');
 
 		console.info("Set the token for the active tab (" + tabToken + ")");
+
     }
 
     $('a[data-toggle="tab"]').on('shown', setTokenForTab);
@@ -218,6 +298,12 @@ require(['jquery','underscore','splunkjs/mvc', 'bootstrap.tab', 'splunkjs/mvc/si
     setActiveTabToken();
 
 });
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    var r = parseInt(result[1], 16),g=parseInt(result[2], 16),b=parseInt(result[3], 16)
+    return result ? r+','+g+','+b : null;
+}
 
 
 
